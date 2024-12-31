70c9bb67018678fe421404b904bab71842c83466
Tamy <tamy8677@gmail.com>
2024-12-31 21:19:46 +0800

    陣列

---
+++ basic_note/array.cs
@@ -0,0 +1,17 @@
+// Array 陣列
+
+// 創建陣列
+int[] scores = {50, 60, 70, 30, 20, 90};
+//   索引值：      0   1   2   3   4   5
+string[] phones = new string[10]; // 創建空陣列，陣列內容預計為10個字串型態的元素
+
+// 取得陣列元素
+System.Console.WriteLine(scores[0]);
+System.Console.WriteLine(scores[1]);
+
+// 修改陣列元素
+scores[0] = 60;
+
+// 新增陣列元素
+phones[0] = "0123456789";
+phones[1] = "088888888";
