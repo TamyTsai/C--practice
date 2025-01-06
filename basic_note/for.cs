1de3290f97c5a3aa8890ea18412398214a88fa56
Tamy <tamy8677@gmail.com>
2025-01-06 22:32:52 +0800

    for迴圈

---
+++ basic_note/for.cs
@@ -0,0 +1,13 @@
+// for 迴圈
+
+for(int i = 1; i <= 5; i++) // 初始值 執行條件 每次迴圈結束後要做的事
+{
+	System.Console.WriteLine(i);
+}
+
+// for 迴圈與陣列
+int[] nums = { 15, 1561, 6, 2, 8156, 156, 626 };
+for(int i = 0; i < nums.Length; i++) // 跑出陣列中所有元素
+{
+	System.Console.WriteLine(nums[i]);
+}
