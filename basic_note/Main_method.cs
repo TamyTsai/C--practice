9c64a46b5ae0e36ced755d81feddb7ccfada4b9e
Tamy <tamy8677@gmail.com>
2025-01-11 19:04:50 +0800

    Main方法

---
+++ basic_note/Main_method.cs
@@ -0,0 +1,15 @@
+// Main方法
+// 程式碼開始執行的地方（進入點）
+// class Program {static void Mian(){...}} // 在執行時C#會自動把程式碼內容 裝進Program類別的Main方法中
+// C#透過Main方法來確定程式由哪裡開始執行，按下執行時，C#會去找Main方法，不用特別呼叫
+
+using ConsleApp1;
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("你好");
+	}
+}
